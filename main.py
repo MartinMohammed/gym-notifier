003157e4cf0fba4f11b3d519e26e0431cf4469ab
Martin Mohammed <martin824616@gmail.com>
2023-05-04 18:59:37 +0200

    change the target_minimum value

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ Studio.instantiate_studios()
 CONFIGURATION
 '''
 TARGET_STUDIO = "Griesheim"
-TARGET_MINIMUM = 50
+TARGET_MINIMUM = 70
 MY_NAME = "Martin"
 TIME_INTERESTED_IN = {"start": 17, "end": 18} # included [start, end + 1)
 
 
 # internal On-going process (infinite while looop!)
-Studio.notify_on_people_amount_criteria(time_interested_in= TIME_INTERESTED_IN, recipient_name= MY_NAME, minimum= TARGET_MINIMUM, specific_studio_name= TARGET_STUDIO, telegram_chat_id= TELEGRAM_CHAT_ID)
+Studio.notify_on_people_amount_criteria(time_interested_in= TIME_INTERESTED_IN, recipient_name= MY_NAME, minimum= TARGET_MINIMUM, maximum= None, specific_studio_name= TARGET_STUDIO, telegram_chat_id= TELEGRAM_CHAT_ID)
