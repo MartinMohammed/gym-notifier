7bc727655ce93366aaa6ee76a41f3a45f2fff0f2
Martin Mohammed <martin824616@gmail.com>
2023-05-07 10:22:21 +0200

    add the new terminal extracted parameters to the notify_on_people function

--- main.py
+++ main.py
@@ -19,12 +19,14 @@ TIME_INTERESTED_IN = {"start": 17, "end": 18} # included [start, end + 1)
 
 try: 
     # SET UP THE PROGRAM -- GET THE REQUIRED INPUT 
+    print(30 * "-")
     target_minimum = int(input("Please enter a target minimum of people in the studio: ")) 
     start_time = int(input("Please enter the start hour (e.g. 17h) you are interested in: "))
     end_time = int(input("Please enter the end hour (e.g. 21h) you are intersted in: "))
+    print(30 * "-")
 except Exception as e: 
     print(f"There was an error with receiving the configuration input:\n {e}")
 
 
 # internal On-going process (infinite while looop!)
-Studio.notify_on_people_amount_criteria(time_interested_in= TIME_INTERESTED_IN, recipient_name= MY_NAME, minimum= TARGET_MINIMUM, maximum= None, specific_studio_name= TARGET_STUDIO, telegram_chat_id= TELEGRAM_CHAT_ID)
+Studio.notify_on_people_amount_criteria(time_interested_in= {"start": start_time or TIME_INTERESTED_IN.get("start"), "end": end_time or TIME_INTERESTED_IN.get("end")}, recipient_name= MY_NAME, minimum= target_minimum or TARGET_MINIMUM, maximum= None, specific_studio_name= TARGET_STUDIO, telegram_chat_id= TELEGRAM_CHAT_ID)
